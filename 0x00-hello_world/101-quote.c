54e5dbefba57aec8919c1b38c315c6d436096cf6
fredrcik <fredrickdivine8@gmail.com>
2023-06-15 14:54:33 -0700

    msssage

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: return 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of is useful \"");
+	fprintf(stderr, " - Dora Korpar , 2015-10-19\n");
+
+	return (1);
+}

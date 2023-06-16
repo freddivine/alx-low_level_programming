566e943fdc0101669fa20243aa948884dbeb89bb
fredrcik <fredrickdivine8@gmail.com>
2023-06-16 10:04:56 -0700

    message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: return 0
+ */
+int main(void)
+{
+	int base, a;
+
+	a = 10;
+
+	for (base = 0; base < a; base++)
+	{
+		printf("%d\n", base);
+	}
+
+	return (0);
+}

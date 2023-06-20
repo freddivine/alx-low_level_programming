7ff8b8e807dd49fe87019e9391ae725db524453d
fredrcik <fredrickdivine8@gmail.com>
2023-06-20 15:52:16 -0700

    message

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print a-z
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}

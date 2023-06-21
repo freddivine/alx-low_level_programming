8eedaca5e0541784620be56762b5dd0d27108276
fredrcik <fredrickdivine8@gmail.com>
2023-06-20 18:25:31 -0700

    message

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -9,13 +9,23 @@ int main(void)
 {
 	int r;
 
-	print_last_digit(98);
+	r = print_last_digit(98);
+	_putchar(r + '0');
 
-	print_last_digit(0);
+	r = print_last_digit(0);
+	_putchar(r + '0');
 
 	r = print_last_digit(-1024);
+	_putchar(r + '0');
 
-	_putchar('0' + r);
+	r = print_last_digit(INT_MAX);
+	_putchar(r + '0');
+
+	r = print_last_digit(INT_MIN);
+	_putchar(r / '0');
+
+	r = print_last_digit(1);
+	_putchar(r + '0');
 
 	_putchar('\n');
 

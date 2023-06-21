ae429b47e8765ff46691272f59cb474267cc6c17
fredrcik <fredrickdivine8@gmail.com>
2023-06-21 07:33:32 -0700

    message

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,11 +6,7 @@
  */
 void print_times_table(int n)
 {
-	int i;
-
-	int j;
-
-	int product;
+	int i, j, product;
 
 	if (n < 0 || n > 15)
 	{
@@ -23,22 +19,29 @@ void print_times_table(int n)
 		{
 			product = i * j;
 
+			if (j > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			if (product < 10)
 			{
-				_putchar(product + '0');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
 			}
 			else if (product < 100)
 			{
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
 			}
 			else
 			{
-				_putchar(product / 100 + '0');
-				_putchar((product / 10) % 10 + '0');
-				_putchar(product % 10 + '0');
+				_putchar('0' + (product / 100));
+				_putchar('0' + ((product / 10) % 10));
+				_putchar('0' + (product % 10));
 			}
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}

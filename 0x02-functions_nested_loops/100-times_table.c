d1a26975b5c5b94f13f4fb26c9921c9bad715a50
fredrcik <fredrickdivine8@gmail.com>
2023-06-21 07:02:00 -0700

    message

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
+{
+	int i;
+
+	int j;
+
+	int product;
+
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			product = i * j;
+
+			if (product < 10)
+			{
+				_putchar(product + '0');
+			}
+			else if (product < 100)
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+			else
+			{
+				_putchar(product / 100 + '0');
+				_putchar((product / 10) % 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}

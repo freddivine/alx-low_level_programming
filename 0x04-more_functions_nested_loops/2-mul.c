1d6989792c61d91114a6c8735505c53957a88f1b
fredrcik <fredrickdivine8@gmail.com>
2023-06-23 05:59:01 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - Entry point
+ * @a: multipled by b
+ * @b: multiplied by a
+ * Return: return mul
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}

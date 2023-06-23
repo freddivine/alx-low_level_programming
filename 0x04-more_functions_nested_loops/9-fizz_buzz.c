95c91f2d486e1aaa191b42aa8865160d96e6c3d7
fredrcik <fredrickdivine8@gmail.com>
2023-06-23 12:52:10 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("FIZZ");
+		}
+
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("BUZZ");
+		}
+
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FIZZBUZZ");
+		}
+
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}

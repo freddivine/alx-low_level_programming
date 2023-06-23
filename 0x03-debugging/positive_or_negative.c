31b863004126c590e2cdaaeaccdc3d916482e254
fredrcik <fredrickdivine8@gmail.com>
2023-06-22 17:40:00 -0700

    Alx 0x03 debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -4,33 +4,26 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
 /**
- * positive_or_negative - initializing main funnction
+ * positive_or_negative - initializing main function
  * @n: parameter to check
- * Return: return 0
+ * Return: returns 0
  */
 int positive_or_negative(int n)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 
 	if (n > 0)
 	{
-		printf("%i is positve\n", n);
+		printf("%i is positive\n", n);
 	}
-
 	else if (n == 0)
 	{
 		printf("%i is zero\n", n);
 	}
-
 	else
 	{
-		("%i is negative\n", n);
+		printf("%i is negative\n", n);
 	}
-
 	return (0);
 }

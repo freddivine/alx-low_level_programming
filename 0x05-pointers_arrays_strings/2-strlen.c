5ba1a82367ad6a33c258d6fbf1d4aadeb46cb176
fredrcik <fredrickdivine8@gmail.com>
2023-06-26 17:27:10 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function for string param
+ * @s: param to check for length
+ * Return: returns string length
+ */
+int _strlen(char *s)
+{
+	int l = strlen(s);
+
+	return (l);
+}

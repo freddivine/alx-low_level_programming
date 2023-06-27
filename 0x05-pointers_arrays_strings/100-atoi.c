21b15b3d8608007e3164d66e35d4603116917db9
fredrcik <fredrickdivine8@gmail.com>
2023-06-27 04:50:30 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _atoi - function
+ * @s: param
+ * Return: return 0
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+
+	int result = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+
+		else if (*s >= '0' && *s <= '9')
+		{
+			result = result * 10;
+
+			result = result + (*s - '0');
+		}
+		
+		else if (result > 0)
+		{
+			break;
+		}
+
+		s++;
+	}
+
+	return (sign * result);
+}

6717b002e77f3f0ab32ae44c43a083b9f8dcb005
fredrcik <fredrickdivine8@gmail.com>
2023-06-29 05:27:18 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - Entry point
+ * @a: array
+ * @n: number of array
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}

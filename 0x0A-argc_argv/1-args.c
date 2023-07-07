103990a94e25eb8c0573ecf616e6d8edda5b2c1b
fredrcik <fredrickdivine8@gmail.com>
2023-07-07 15:38:13 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: argument vector
  * Return: return 0
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char  __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc);
 	return (0);

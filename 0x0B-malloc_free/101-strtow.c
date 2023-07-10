8cf44a49ca511d4ef8e31a48378c0139f84639a6
fredrcik <fredrickdivine8@gmail.com>
2023-07-10 07:22:47 -0700

    0x00 malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,7 +16,7 @@ char **free_memory(char **words, int index);
  */
 int is_space(char c)
 {
-	return (c == ' ' || c == '\t' || c == '\n');
+    return (c == ' ' || c == '\t' || c == '\n');
 }
 
 /**
@@ -27,18 +27,18 @@ int is_space(char c)
  */
 int count_words(char *str)
 {
-	int i, count, len;
+    int i, count, len;
 
-	len = strlen(str);
-	count = 0;
+    len = strlen(str);
+    count = 0;
 
-	for (i = 0; i < len; i++)
-	{
-		if (!is_space(str[i]) && (is_space(str[i + 1]) || str[i + 1] == '\0'))
-			count++;
-	}
+    for (i = 0; i < len; i++)
+    {
+        if (!is_space(str[i]) && (is_space(str[i + 1]) || str[i + 1] == '\0'))
+            count++;
+    }
 
-	return (count);
+    return (count);
 }
 
 /**
@@ -49,44 +49,43 @@ int count_words(char *str)
  */
 char **strtow(char *str)
 {
-	char **words;
-	int i, j, k, len, word_count;
+    char **words;
+    int i, j, k, len, word_count;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
+    if (str == NULL || *str == '\0')
+        return (NULL);
 
-	word_count = count_words(str);
-	words = malloc((word_count + 1) * sizeof(char *));
-	if (words == NULL)
-		return (NULL);
+    word_count = count_words(str);
+    words = malloc((word_count + 1) * sizeof(char *));
+    if (words == NULL)
+        return (NULL);
 
-	for (i = 0, j = 0; i < word_count; i++)
-	{
-		while (is_space(str[j]))
-			j++;
+    for (i = 0, j = 0; i < word_count; i++)
+    {
+        while (is_space(str[j]))
+            j++;
 
-		len = 0;
-		while (!is_space(str[j + len]) && str[j + len] != '\0')
-			len++;
+        len = 0;
+        while (!is_space(str[j + len]) && str[j + len] != '\0')
+            len++;
 
-		if (len == 0)
-		{
-			i--;
-			continue;
-		}
+        words[i] = malloc((len + 1) * sizeof(char));
+        if (words[i] == NULL)
+            return (free_memory(words, i));
 
-		words[i] = malloc((len + 1) * sizeof(char));
-		if (words[i] == NULL)
-			return (free_memory(words, i));
+        for (k = 0; k < len; k++)
+            words[i][k] = str[j++];
 
-		for (k = 0; k < len; k++)
-			words[i][k] = str[j++];
+        words[i][k] = '\0';
+    }
 
-		words[i][k] = '\0';
-	}
+    words[i] = malloc(sizeof(char));
+    if (words[i] == NULL)
+        return (free_memory(words, i));
 
-	words[i] = NULL;
-	return (words);
+    words[i][0] = '\0';
+
+    return (words);
 }
 
 /**
@@ -97,22 +96,22 @@ char **strtow(char *str)
  */
 char *get_word(char *str)
 {
-	int i, len;
-	char *word;
+    int i, len;
+    char *word;
 
-	len = 0;
-	while (!is_space(str[len]) && str[len] != '\0')
-		len++;
+    len = 0;
+    while (!is_space(str[len]) && str[len] != '\0')
+        len++;
 
-	word = malloc((len + 1) * sizeof(char));
-	if (word == NULL)
-		return (NULL);
+    word = malloc((len + 1) * sizeof(char));
+    if (word == NULL)
+        return (NULL);
 
-	for (i = 0; i < len; i++)
-		word[i] = str[i];
+    for (i = 0; i < len; i++)
+        word[i] = str[i];
 
-	word[i] = '\0';
-	return (word);
+    word[i] = '\0';
+    return (word);
 }
 
 /**
@@ -124,11 +123,11 @@ char *get_word(char *str)
  */
 char **free_memory(char **words, int index)
 {
-	int i;
+    int i;
 
-	for (i = 0; i <= index; i++)
-		free(words[i]);
+    for (i = 0; i <= index; i++)
+        free(words[i]);
 
-	free(words);
-	return (NULL);
+    free(words);
+    return (NULL);
 }

1041a06a8e604fa0a5fc4e0f6c3a02ba14e8071e
freddivine <fredrickdivine8@gmail.com>
2023-08-03 07:24:30 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,10 +15,10 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while (*b != '\0');
+	while (*b != '\0')
 	{
 		if (*b != '0' && *b != '1')
-			return 0;
+			return (0);
 
 		result = (result * 2) + (*b - '0');
 		b++;

db74412971a4c982a8bb12a7db70bb98c4e34a2f
freddivine <fredrickdivine8@gmail.com>
2023-08-03 07:34:21 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,13 +9,13 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int dog = 1;
+	unsigned long int mask = 1;
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	mask <<= index;
-	*n |= dog;
+	*n |= mask;
 
 	return (1);
 }
